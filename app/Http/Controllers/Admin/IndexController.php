fea2bf2518e5fdaddfe74836156676c87f077e76
846953760 <846953760@qq.com>
2019-02-02 15:35:39 +0800

    HTTP接收请求及返回响应$request\response 重定向redirect 下载文件download()

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -14,8 +14,31 @@ class IndexController extends Controller
      */
     public function index()
     {
-        // echo 'Admin/Index/index';
-        return view('admin.index');
+        // return "Hello world";   //return 字符串
+        // return array('a'=>1,'b'=>2,'c'=>3);     //return 数组，但是框架会自动转为json
+        // return response('hello world', 200);        //自定义响应值和响应状态码
+        // return response(array('a'=>1,'b'=>2,'c'=>3),200)->header('Content-Type','application/json');    //自定义响应值、响应状态码、响应类型
+        
+        //链式写法1，可以设置多值
+        // return response(array('a'=>1,'b'=>2,'c'=>3),200)
+        //             ->header('Content-Type','application/json')
+        //             ->header('X-Header-One','Header value 1')
+        //             ->header('X-Header-Two','Header value 2');
+        //链式写法2，使用withHeaders数组
+        // return response(array('a'=>1,'b'=>2,'c'=>3),200)
+        //             ->withHeaders([
+        //                 'Content-Type'=>'application/json',
+        //                 'X-Header-One'=>'Header value 1',
+        //                 'X-Header-Two'=>'Header value 2'
+        //             ]);
+        // return response('hello world', 200)->cookie('name','value',1);  //设置响应的cookie,该cookie被openssl_encrypt加密,如果不想被加密,可以去EncryptCookies.php添加该cookie的name
+        // return redirect('/home/index/show/1');      //重定向到/home/index/show/1中，注意必须在route中配置/home/index/show/{id}
+        // return redirect()->away('https://www.baidu.com');    //重定向到外部地址，得使用away
+        // return view('admin.index');
+        // return response()->view('admin.index',array('a','b'),200)->header('Content-Type','text/plain');
+        // return response()->json(array('a'=>1,'b'=>2,'c'=>3));
+        // return response()->json(['name'=>'lisi','age'=>20])->withCallback($request->input('callback')); //创建jsonp响应
+        return response()->download(public_path().'\robots.txt','111.txt');     //下载文件，第一个参数为文件的绝对路径,第二个参数为下载时要显示的文件名,第三个参数可以设置header头
     }
 
     /**
